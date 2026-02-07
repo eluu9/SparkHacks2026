99a5bb03ee53a7e383028562c9db8211b2f7b720
Ethan Luu <ethan.luu71@gmail.com>
2026-02-07 01:17:58 -0600

    Finished Match Services

--- app/services/match_service.py
+++ app/services/match_service.py
@@ -8,7 +8,7 @@ def scoreTier1(kitItem, searchItem):
     normalizedKitUPC = normalizeUPCAndEPN(kitUPC)
     normalizedSearchUPC = normalizeUPCAndEPN(searchUPC)
     if normalizedKitUPC and normalizedSearchUPC and normalizedKitUPC == normalizedSearchUPC:
-        return (0.95, f"Exact UPC Match: {normalizedKitUPC}")
+        return (0.95, [f"Exact UPC Match"])
     return (0, [])
 
 def scoreTier2(kitItem, searchItem):
@@ -17,14 +17,14 @@ def scoreTier2(kitItem, searchItem):
     normalizedKitMPN = normalizeMPN(kitMPN)
     normalizedSearchMPN = normalizeMPN(searchMPN)
 
-    kitBrand = kitItem['identifier_hints']['brand']
-    searchBrand = searchItem['brand']
-    normalizedKitBrand = normalizeBrand(kitBrand)
-    normalizedSearchBrand = normalizeBrand(searchBrand)
+    #kitBrand = kitItem['identifier_hints']['brand']
+    #searchBrand = searchItem['brand']
+    #normalizedKitBrand = normalizeBrand(kitBrand)
+    #normalizedSearchBrand = normalizeBrand(searchBrand)
 
     if normalizedKitMPN == normalizedSearchMPN:
-        if normalizedKitBrand == normalizedSearchBrand:
-            return (0.88, ["Exact MPN and Brand Match"])
+        #if normalizedKitBrand == normalizedSearchBrand:
+        #    return (0.88, ["Exact MPN and Brand Match"])
         return (0.75, ["Exact MPN Match"])
     return (0, [])
 
@@ -34,23 +34,40 @@ def scoreTier3(kitItem, searchItem):
     normalizedKitModel = normalizeModel(kitModel)
     normalizedSearchModel = normalizeModel(searchModel)
 
-    kitBrand = kitItem['identifier_hints']['brand']
-    searchBrand = searchItem['brand']
-    normalizedKitBrand = normalizeBrand(kitBrand)
-    normalizedSearchBrand = normalizeBrand(searchBrand)
+    #kitBrand = kitItem['identifier_hints']['brand']
+    #searchBrand = searchItem['brand']
+    #normalizedKitBrand = normalizeBrand(kitBrand)
+    #normalizedSearchBrand = normalizeBrand(searchBrand)
 
     if normalizedKitModel == normalizedSearchModel:
-        if normalizedKitBrand == normalizedSearchBrand:
-            return (0.82, ["Exact Model and Brand Match"])
+        #if normalizedKitBrand == normalizedSearchBrand:
+        #    return (0.82, ["Exact Model and Brand Match"])
         return (0.70, ["Exact Model Match"])
     return (0, [])
 
 def scoreTier4(kitItem, searchItem):
     kitName = kitItem['name']
+    kitSpecs = kitItem['specs_to_search']
     searchName = searchItem['title']
-    normalizedKitName = normalizeBrand(kitName)
-    normalizedSearchName = normalizeBrand(searchName)
-    return (difflib.SequenceMatcher(None, normalizedKitName, normalizedSearchName).ratio(), ["No Exact Name Match"])
+    normalizedKitName = normalizeString(kitName)
+    normalizedSearchName = normalizeString(searchName)
+
+    similarity = difflib.SequenceMatcher(None, normalizedKitName, normalizedSearchName).ratio()
+    if similarity < 0.60:
+        return(0, [])
+
+    specsFound = 0
+    for spec in kitSpecs:
+        normalizedSpecs = normalizeString(spec)
+        if (normalizedSpecs in normalizedSearchName):
+            specsFound += 1
+    if similarity > 0.85 and specsFound > 0:
+        return (0.70, ["High Title Similarity with Specs Found"])
+    elif similarity > 0.80 and specsFound >= 1:
+        return (0.65, ["Medium Title Similarity with Specs Found"])
+    elif similarity > 0.75:
+        return (0.50, ["Low Title Similarity with No Specs Found"])
+    return (0, [])
 
 def calculateConfidence(kitItem, searchItem):
     confidence = 0
